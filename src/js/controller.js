6fdf8fefa56f3e6717c49edad98a6c8e0b4a30c3
Omar <omared250@gmail.com>
2022-08-23 16:02:31 +0200

    win. draw and lose view

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,10 +1,25 @@
+import decisionView from "./views/decisionView.js";
 import pickedView from "./views/pickedView.js";
 
 const controlPick = function() {
     const userChoice = document.querySelector('.user__choice').querySelector('img');
     const pcChoice = document.querySelector('.pc__choice').querySelector('img');
 
-    
+    if (userChoice.id === pcChoice.id) {
+        decisionView.renderDecision('DRAW')
+    } else if (userChoice.id === 'paper' & pcChoice.id === 'rock') {
+        decisionView.renderDecision('WIN')
+    } else if (userChoice.id === 'paper' & pcChoice.id === 'scissors') {
+        decisionView.renderDecision('LOSE')
+    } else if (userChoice.id === 'scissors' & pcChoice.id === 'paper') {
+        decisionView.renderDecision('WIN')
+    } else if (userChoice.id === 'scissors' & pcChoice.id === 'rock') {
+        decisionView.renderDecision('LOSE')
+    } else if (userChoice.id === 'rock' & pcChoice.id === 'scissors') {
+        decisionView.renderDecision('WIN')
+    } else if (userChoice.id === 'rock' & pcChoice.id === 'paper') {
+        decisionView.renderDecision('LOSE')
+    }
 }
 
 const init = function() {
--- src/js/views/decisionView.js
+++ src/js/views/decisionView.js
@@ -1,13 +1,15 @@
 class decisionView {
-    _parentEl = document.querySelector('.descision__container');
+    _parentEl = document.querySelector('.decision__container');
 
-    renderDecision() {
-
+    renderDecision(decision) {
+        const markup = this._generateMarkupDecision(decision);
+        this._parentEl.classList.remove('hidden');
+        this._parentEl.insertAdjacentHTML('afterbegin', markup);
     }
 
-    _generateMarkupDecision() {
+    _generateMarkupDecision(decision) {
         return `
-        <span class="decision">YOU WIN</span>
+        <span class="decision">${decision === 'DRAW' ? 'DRAW' : 'YOU' + ' ' + decision}</span>
         <button class="play__again__btn">PLAY AGAIN</button>
         `
     }
