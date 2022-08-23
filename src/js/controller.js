0e237804f2a393f354ae1d49004b8def03c4756a
Omar <omared250@gmail.com>
2022-08-23 14:51:10 +0200

    picked view

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,10 +1,7 @@
 import pickedView from "./views/pickedView.js";
 
-const controlPick = function(picked) {
-    const userChoice = document.querySelector('.user__choice');
-    console.log(userChoice);
-    console.log(picked.src);
-    userChoice.insertAdjacentHTML('afterbegin', `<img class="scissors__picked" src="${picked.src}" alt="scissors image" id="scissors">`);
+const controlPick = function() {
+    
 }
 
 const init = function() {
--- src/js/views/pickedView.js
+++ src/js/views/pickedView.js
@@ -10,26 +10,55 @@ class pickedView {
 
                 if (e.target.id === 'rock') {
                     this._parentEl.innerHTML = '';
-                    this.renderPicked();
-                    handler(e.target)
+                    this._renderPicked();
+                    this._renderUserChoice(e.target);
+                    this._renderComputerChoice();
 
                 } else if (e.target.id === 'scissors') {
                     this._parentEl.innerHTML = '';
-                    this.renderPicked();
-                    handler(e.target)
+                    this._renderPicked();
+                    this._renderUserChoice(e.target);
+                    this._renderComputerChoice();
 
                 } else if (e.target.id === 'paper') {
                     this._parentEl.innerHTML = '';
-                    this.renderPicked();
-                    handler(e.target)
+                    this._renderPicked();
+                    this._renderUserChoice(e.target);
+                    this._renderComputerChoice();
                 }
+
+                handler()
             })
         })
     }
 
-    renderPicked() {
+    _renderPicked() {
         const markup = this._generateMarkupPicked();
         this._parentEl.insertAdjacentHTML('beforeend', markup);
+    }
+
+    _renderUserChoice(picked) {
+        const userChoice = document.querySelector('.user__choice');
+        userChoice.insertAdjacentHTML('afterbegin', `<img class="${picked.id}__picked" src="${picked.src}" alt="${picked.id} image" id="${picked.id}">`);
+    }
+
+    _renderComputerChoice() {
+        const pcChoice = document.querySelector('.pc__choice');
+        const figure = document.querySelector('.figure');
+        const choice = Math.trunc(Math.random() * 3) + 1;
+
+        let markup;
+        
+        if (choice === 1) {
+            markup = `<img class="paper__picked animation" src="./src/images/icon-paper.svg" alt="paper image" id="paper">`
+        } else if (choice === 2) {
+            markup = `<img class="scissors__picked animation" src="./src/images/icon-scissors.svg" alt="scissors image" id="scissors">`
+        } else if (choice === 3) {
+            markup = `<img class="rock__picked animation" src="./src/images/icon-rock.svg" alt="rock image" id="rock">`
+        }
+
+        figure.style.display = 'none';
+        pcChoice.insertAdjacentHTML('afterbegin', markup);
     }
 
     _generateMarkupPicked() {
@@ -38,6 +67,7 @@ class pickedView {
             <p>YOU PICKED</p>
         </div>
         <div class="pc__choice">
+            <span class="figure"></span>
             <p>THE HOUSE PICKED</p>
         </div>
         `
