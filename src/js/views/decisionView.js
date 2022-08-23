9b31e9db0edbfa0d709b0bfd8273d8b7315537f4
Omar <omared250@gmail.com>
2022-08-23 15:29:43 +0200

    updating view decision

--- src/js/views/decisionView.js
+++ src/js/views/decisionView.js
@@ -1,6 +1,16 @@
 class decisionView {
     _parentEl = document.querySelector('.descision__container');
-    
+
+    renderDecision() {
+
+    }
+
+    _generateMarkupDecision() {
+        return `
+        <span class="decision">YOU WIN</span>
+        <button class="play__again__btn">PLAY AGAIN</button>
+        `
+    }
 }
 
 export default new decisionView();
