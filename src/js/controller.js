4283bbf99469f6b483db27e73b2cd7d8408c477c
Omar <omared250@gmail.com>
2022-08-23 16:40:54 +0200

    score view

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,25 +1,47 @@
 import decisionView from "./views/decisionView.js";
 import pickedView from "./views/pickedView.js";
+import * as model from "./model.js"
 
 const controlPick = function() {
     const userChoice = document.querySelector('.user__choice').querySelector('img');
     const pcChoice = document.querySelector('.pc__choice').querySelector('img');
 
+    controlScore(model.score.currentScore);
+
     if (userChoice.id === pcChoice.id) {
-        decisionView.renderDecision('DRAW')
+        decisionView.renderDecision('DRAW');
+
     } else if (userChoice.id === 'paper' & pcChoice.id === 'rock') {
-        decisionView.renderDecision('WIN')
+        decisionView.renderDecision('WIN');
+        model.score.currentScore++;
+
     } else if (userChoice.id === 'paper' & pcChoice.id === 'scissors') {
-        decisionView.renderDecision('LOSE')
+        decisionView.renderDecision('LOSE');
+        model.score.currentScore <= 0 ? model.score.currentScore : model.score.currentScore--; 
+
     } else if (userChoice.id === 'scissors' & pcChoice.id === 'paper') {
-        decisionView.renderDecision('WIN')
+        decisionView.renderDecision('WIN');
+        model.score.currentScore++;
+
     } else if (userChoice.id === 'scissors' & pcChoice.id === 'rock') {
-        decisionView.renderDecision('LOSE')
+        decisionView.renderDecision('LOSE');
+        model.score.currentScore <= 0 ? model.score.currentScore : model.score.currentScore--; 
+
     } else if (userChoice.id === 'rock' & pcChoice.id === 'scissors') {
-        decisionView.renderDecision('WIN')
+        decisionView.renderDecision('WIN');
+        model.score.currentScore++;
+
     } else if (userChoice.id === 'rock' & pcChoice.id === 'paper') {
-        decisionView.renderDecision('LOSE')
+        decisionView.renderDecision('LOSE');
+        model.score.currentScore <= 0 ? model.score.currentScore : model.score.currentScore--;
     }
+
+    controlScore(model.score.currentScore);
+}
+
+const controlScore = function(scoreNumber) {
+    const score = document.querySelector('.score__number');
+    score.textContent = scoreNumber;
 }
 
 const init = function() {
